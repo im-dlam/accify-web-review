dfb68ebc59c1a003b77309881e832bc2185b6c5a
Lam <ldl.contact.booking@gmail.com>
2026-02-02 23:41:02 +0700

    refactor(models): add new table user_purchases

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 
 from aiohttp import TraceRequestChunkSentParams
+from regex import T
 from sqlalchemy import (
     Column, String, Integer, ForeignKey, Boolean, Enum,
     DateTime, Date ,Float
@@ -10,8 +11,8 @@ from datetime import datetime
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String, unique=True)
-    email = Column(String, unique=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
     password_hash = Column(String, nullable=False)
     full_name = Column(String, default="")
     role = Column(Enum("admin","member","distributor","collaborator", name="user_role") , default="member")
@@ -33,7 +34,7 @@ class Product(Base):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True, autoincrement=True)
     category_id = Column(Integer, ForeignKey('product_categories.id'), index=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, index=True)
     price = Column(Float, default=999999)
     stock = Column(Integer, default=0)
     sold = Column(Integer, default=0)
@@ -51,14 +52,15 @@ class ProductInventory(Base):
     __tablename__ = "product_inventories"
     id = Column(Integer, primary_key=True, autoincrement=True)
     product_id = Column(Integer, ForeignKey('products.id'), index=True)
-    product = Column(String, unique=True, index=True)
+    product = Column(String, unique=True)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
+    
 class Wallet(Base):
     __tablename__ = "wallets"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), unique=True)
+    user_id = Column(Integer, ForeignKey('users.id'), unique=True, index=True)
     balance = Column(Float, default=0)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
@@ -66,8 +68,20 @@ class Wallet(Base):
 class Payment(Base):
     __tablename__ = "payments"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    wallet_id = Column(Integer, ForeignKey("wallets.id"), unique=True)
+    wallet_id = Column(Integer, ForeignKey("wallets.id"))
     amount = Column(Float)
     status = Column(Enum("pending","success","failed", name="payment_status"))
     description = Column(String, default="")
     created_at = Column(DateTime, default=datetime.now)
+    
+    
+class UserPurchase(Base):
+    __tablename__ = "user_purchases"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    name = Column(String)
+    quantity = Column(Integer)
+    total_price = Column(Integer)
+    product = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=True)    
