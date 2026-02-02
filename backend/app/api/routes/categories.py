33ca1789297eecf10304e3df64175c407bd5da7d
Lam <ldl.contact.booking@gmail.com>
2026-02-02 21:14:41 +0700

    fix(categories, products): fix delete item route

--- backend/app/api/routes/categories.py
+++ backend/app/api/routes/categories.py
@@ -1,18 +1,19 @@
 from typing import Annotated, Any
 from fastapi import APIRouter, Depends, status, Body
-from app.api.deps import get_db , get_current_user , get_current_admin
+from app.api.deps import get_db, get_current_user, get_current_admin
 from app.api.exception import APIException
-from app.schemas import CategoryPublic , Category , CategoryCreate, ItemDelete , UserPublic
+from app.schemas import CategoryPublic, Category, CategoryCreate, ItemDelete, UserPublic
 from app.models import ProductCategory, Product
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select , update
+from sqlalchemy import select, update
 from sqlalchemy.orm import load_only
 from sqlalchemy.exc import IntegrityError
 
 router = APIRouter(prefix="/categories", tags=["category"])
 
+
 @router.get("/", response_model=CategoryPublic)
-async def get_categories(db: AsyncSession =  Depends(get_db)):
+async def get_categories(db: AsyncSession = Depends(get_db)):
     """Lấy danh sách loại mặt hàng
 
     Args:
@@ -20,57 +21,71 @@ async def get_categories(db: AsyncSession =  Depends(get_db)):
     """
     stmt = (
         select(ProductCategory)
-        .options(load_only(
-            ProductCategory.id, ProductCategory.name,
-            ProductCategory.keyword, ProductCategory.color
-        ))
+        .options(
+            load_only(
+                ProductCategory.id,
+                ProductCategory.name,
+                ProductCategory.keyword,
+                ProductCategory.color,
+            )
+        )
         .offset(0)
         .limit(100)
     )
-    
+
     result = await db.execute(stmt)
     categories = result.scalars().all()
-    
+
     return CategoryPublic(success=True, categories=categories)
+
 
 @router.post("/", response_model=Category, status_code=201)
 async def create_category(
-    *, 
-    category : CategoryCreate, 
-    db:AsyncSession = Depends(get_db), 
-    current_user: Annotated[UserPublic ,Depends(get_current_admin)]):
-    
+    *,
+    category: CategoryCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: Annotated[UserPublic, Depends(get_current_admin)],
+):
+    """Tạo danh mục mới"""
+
     category_new = ProductCategory(**category.model_dump())
     try:
         db.add(category_new)
         await db.commit()
-        await db.refresh(category_new)    
+        await db.refresh(category_new)
     except IndentationError as e:
         await db.rollback()
         msg = str(e.orig).lower()
         if "name" in msg:
             raise APIException(status_code=400, message="Name already exists")
-        
+
         if "keyword" in msg:
             raise APIException(status_code=400, message="Keyword already exists")
-        
-        raise APIException(status_code=400, message="Please double check your name or keyword.")
-        
-        
+
+        raise APIException(
+            status_code=400, message="Please double check your name or keyword."
+        )
+
     return category_new
+
 
 @router.delete("/{category_id}", response_model=ItemDelete)
 async def delete_category(
     *,
     category_id: int,
-    db:AsyncSession = Depends(get_db),
-    current_user: Annotated[UserPublic , Depends(get_current_admin)]) -> Any:
-    
-    result = await db.execute(select(ProductCategory).where(ProductCategory.id == category_id))
+    db: AsyncSession = Depends(get_db),
+    current_user: Annotated[UserPublic, Depends(get_current_admin)],
+) -> Any:
+    """Xóa danh mục theo id"""
+
+    result = await db.execute(
+        select(ProductCategory).where(ProductCategory.id == category_id)
+    )
     category = result.scalar_one_or_none()
-    
+    if not category:
+        raise APIException(status_code=404, message="Category not found")
+
     category_data = Category.model_validate(category)
-    category_name = category.name
     if not category:
         raise APIException(status_code=404, message="Not found category")
     try:
@@ -79,32 +94,44 @@ async def delete_category(
     except IntegrityError as e:
         await db.rollback()
         msg = str(e.orig).lower()
-        
+
         if "foreignkeyviolationerror" in msg:
-            raise APIException(status_code=400, message="The category still has products. Please move or delete them first.")
-    
-    return ItemDelete(message=f"{category_name} category removed", item=category_data)
+            raise APIException(
+                status_code=400,
+                message="The category still has products. Please move or delete them first.",
+            )
+
+    return ItemDelete(message="Item deleted successfully", item=category_data)
+
 
 @router.put("/", response_model=Category)
 async def update_category(
     *,
     category: Category,
     db: AsyncSession = Depends(get_db),
-    user: Annotated[UserPublic, Depends(get_current_admin)]):
-    
-    if user.role != "admin":
-        raise APIException(status_code=status.HTTP_423_LOCKED, message="Access is not permitted")
+    user: Annotated[UserPublic, Depends(get_current_admin)],
+):
+    """Cập nhật sản phẩm theo id"""
+    filtered_category = {k: v for k, v in category.model_dump().items() if v != None}
     stmt = (
-        update(ProductCategory).where(ProductCategory.id == category.id).values(category.model_dump())
+        update(ProductCategory)
+        .where(ProductCategory.id == category.id)
+        .values(filtered_category)
     )
-    
-    await db.execute(stmt)
-    await db.commit()
 
-    result = await db.execute(select(ProductCategory).where(ProductCategory.id == category.id))
-    
+    try:
+        await db.execute(stmt)
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        raise APIException(status_code=400, message="Duplicate value or invalid data")
+
+    result = await db.execute(
+        select(ProductCategory).where(ProductCategory.id == category.id)
+    )
+
     updated_category = result.scalar_one_or_none()
     if not updated_category:
         raise APIException(status_code=404, message="Category not found")
-    
+
     return updated_category
--- backend/app/api/routes/products.py
+++ backend/app/api/routes/products.py
@@ -1,19 +1,25 @@
-from fastapi import APIRouter, Depends, status 
+from fastapi import APIRouter, Depends, Request, status, Body
 from fastapi.responses import Response
-from app.api.deps import get_db, get_current_user
-from app.schemas import Product as ProductSchemas, ProductPublic, UserPublic , ProductCreate
-from app.models import Product  as ProductModels
-from sqlalchemy import select
+from app.api.deps import get_db, get_current_user, get_current_admin
+from app.schemas import (
+    Product as ProductSchemas,
+    ProductPublic,
+    UserPublic,
+    ProductCreate,
+    ItemDelete,
+)
+from app.models import Product as ProductModels
+from sqlalchemy import select, update
 from sqlalchemy.orm import load_only
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.exception import APIException
 from sqlalchemy.exc import IntegrityError
+from typing import Annotated
 
 router = APIRouter(prefix="/products", tags=["product"])
 
 
-
-@router.get('/')
+@router.get("/")
 async def get_products(db: AsyncSession = Depends(get_db)) -> ProductPublic:
     """
     Lấy danh sách sản phẩm
@@ -21,35 +27,48 @@ async def get_products(db: AsyncSession = Depends(get_db)) -> ProductPublic:
     stmt = (
         select(ProductModels)
         # Load các column chỉ định tránh load hết lag
-        .options(load_only(
-                ProductModels.id, ProductModels.name, ProductModels.subtitle,
-                 ProductModels.description, ProductModels.stock, ProductModels.price,
-                 ProductModels.discount, ProductModels.country, ProductModels.icons,
-                 ProductModels.time, ProductModels.image_url
-        ))
+        .options(
+            load_only(
+                ProductModels.id,
+                ProductModels.name,
+                ProductModels.subtitle,
+                ProductModels.description,
+                ProductModels.stock,
+                ProductModels.price,
+                ProductModels.discount,
+                ProductModels.country,
+                ProductModels.icons,
+                ProductModels.time,
+                ProductModels.image_url,
+            )
+        )
         # Số bản ghi bỏ qua trước khi lấy kết quả
         .offset(0)
         # Sau này scale thêm phân trang sẽ sửa sau
         .limit(100)
     )
-    
+
     result = await db.execute(stmt)
     items = result.scalars().all()
 
     return ProductPublic(success=True, products=items)
 
-@router.post('/', response_model=ProductSchemas, status_code=201)
+
+@router.post("/", response_model=ProductSchemas, status_code=201)
 async def create_product(
     *,
-    product: ProductCreate,
+    product: ProductCreate = Body(...),
     db: AsyncSession = Depends(get_db),
-    user: UserPublic = Depends(get_current_user)
-    ) -> ProductSchemas:
+    user: UserPublic = Depends(get_current_user),
+) -> ProductSchemas:
     """
     Tạo sản phẩm
     """
+
     if user.role != "admin":
-        raise APIException(status_code=status.HTTP_423_LOCKED, message="Access is not permitted")
+        raise APIException(
+            status_code=status.HTTP_423_LOCKED, message="Access is not permitted"
+        )
     product_new = ProductModels(**product.model_dump())
     try:
         db.add(product_new)
@@ -57,21 +76,78 @@ async def create_product(
         await db.refresh(product_new)
     except IntegrityError as e:
         msg = str(e.orig).lower()
-        
-        raise APIException(status_code=400, message=msg)
-    
+        if "foreignkeyviolationerror" in msg:
+            raise APIException(
+                status_code=400, message="Please double check category_id"
+            )
 
-    
+        raise APIException(status_code=400, message=msg)
+
     return product_new
 
-@router.delete('/{id}')
-async def delete_product():
+
+@router.delete("/{product_id}", response_model=ItemDelete)
+async def delete_product(
+    *,
+    product_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: Annotated[UserPublic, Depends(get_current_admin)],
+):
     """
     Xóa sản phẩm theo id
     """
+    result = await db.execute(
+        select(ProductModels).where(ProductModels.id == product_id)
+    )
+    product = result.scalar_one_or_none()
+    if not product:
+        raise APIException(status_code=404, message="Not found product")
 
-@router.put('/{id}')
-async def update_product():
-    """
-    Cập nhật sản phẩm
-    """
+    product_data = ProductSchemas.model_validate(product)
+
+    try:
+        await db.delete(product)
+        await db.commit()
+    except IntegrityError as e:
+        msg = str(e.orig).lower()
+        if "foreignkeyviolationerror" in msg:
+            raise APIException(
+                status_code=400,
+                message="The product still has accounts. Please move or delete them first.",
+            )
+
+    return ItemDelete(message="Item deleted successfully", item=product_data)
+
+
+@router.put("/")
+async def update_product(
+    *,
+    product: ProductSchemas,
+    db: AsyncSession = Depends(get_db),
+    user: Annotated[UserPublic, Depends(get_current_admin)],
+):
+    """Cập nhật sản phẩm"""
+    filtered_product = {k: v for k, v in product.model_dump().items() if v != None}
+
+    stmt = (
+        update(ProductModels)
+        .where(ProductModels.id == product.id)
+        .values(filtered_product)
+    )
+
+    try:
+        await db.execute(stmt)
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        raise APIException(status_code=400, message="Duplicate value or invalid data")
+
+    result = await db.execute(
+        select(ProductModels).where(ProductModels.id == product.id)
+    )
+
+    updated_product = result.scalar_one_or_none()
+    if not updated_product:
+        raise APIException(status_code=404, message="Product not found")
+
+    return updated_product
